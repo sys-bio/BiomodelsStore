8c2ed52040322903fc30315ed3f6cc3cfe0b5c41
Anish Konanki <anishkonanki@gmail.com>
2024-02-12 23:22:25 -0800

    Updated biomodel download script

--- biomodels.py
+++ biomodels.py
@@ -1,21 +1,48 @@
+import os
 import zipfile
 import requests
 import io
+from biomodels_restful_api_client import services as bmservices
+
+"""
+This script is used to download all the models from BioModels database.
+The models are downloaded in the 'biomodels' directory.
+The error log is written to 'error_log.txt' file.
+"""
 
 error_log = open('error_log.txt', 'w')
 
+# Download the SBML model from BioModels database using the model_id
 def download_biomodels(model_id):
     url = f'https://www.ebi.ac.uk/biomodels/search/download?models={model_id}'
     response = requests.get(url, stream=True)
     extract = zipfile.ZipFile(io.BytesIO(response.content))
+    while extract.namelist()[0].endswith('.zip'):
+        extract = zipfile.ZipFile(io.BytesIO(extract.read(extract.namelist()[0])))
+    print(extract.namelist())
     filename = extract.namelist()[0]
-    if filename.lower().endswith('.omex'):
-        error_log.write(extract.namelist()[0] + '\n')
-        error_log.write(f'{model_id} is omex\n')
+    if not filename.lower().endswith('.xml') and not filename.lower().endswith('.sbml'):
+        print(f'{model_id} is not an SBML file')
+        error_log.write(f'{model_id} is not an SBML file\n')
+        error_log.write('\n')
         return
     extract.extractall('biomodels')
+    print(f'{model_id} downloaded successfully')
     extract.close()
-    
+
 
 if __name__ == '__main__':
-    download_biomodels('MODEL9811206584')
+    model_ids = bmservices.get_model_identifiers()
+
+    # Download all the models from BioModels database using the model_id
+    for model_id in model_ids["models"]:
+        try:
+            if f"{model_id}.xml" in os.listdir("biomodels"):
+                print(f'{model_id} already downloaded')
+                continue
+            download_biomodels(model_id)
+        except Exception as e:
+            error_log.write(model_id + '\n')
+            error_log.write(str(e) + '\n')
+            error_log.write('\n')
+            continue
